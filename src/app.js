7d7885c28d03bb9dc8cd8f8e373ee2879cf7e86a
Jovany1543 <christian.marrero3@gmail.com>
2021-06-24 20:28:18 +0000

    creating repo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector(".card");
+  let item = document.querySelector(".card h1");
+
+  let suit = ["hearts", "diamonds", "spades", "clubs"];
+  let rank = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let selectedSuit = suit[getRndInteger(0, suit.length - 1)];
+
+  card.classList.add(selectedSuit);
+  item.innerHTML = rank[getRndInteger(0, rank.length - 1)];
 };
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
